20c59ec031e17844437c15f60cf16f03bda509d2
npross <npross@lbl.gov>
2016-09-06 16:28:15 +0100

    plots/color_color

---
+++ plots/color_color/readin2.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env
+
+"""
+
+"""
+
+from __future__ import division
+from numpy.random import randn
+from pandas import DataFrame, Series
+from scipy.stats import gaussian_kde
+
+import os
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+path = '/cos_pc19a_npr/data/WISE/W4/WISE_W4_W4SNRge3_W4MPROlt4.0_nohdr.tbl'
+
+
+cols = ['designation', 'ra', 'dec', 'sigra', 'sigdec', 'w1mpro', 'w1sigmpro', 'w1snr', 'w2mpro', 'w2sigmpro', 'w2snr', 'w3mpro', 'w3sigmpro', 'w3snr', 'w4mpro', 'w4sigmpro', 'w4snr', 'w4rchi2']
+
+## If using a file *WITH* a header
+# df = pd.read_table(data_in, header=54) 
+
+## "Trick" is to use the  delimiter='\s+'  option
+data = pd.read_table(path, delimiter='\s+')
+
+"""
+Hi Nic,
+
+Pandas is awesome and I'd recommend it to anyone. In this specific case I'm not exactly sure how you'd read in but looking up on the read_table docs it looks alright, but once you have the dataframe you access the columns by data[column_name] which are (if it has been read in correctly) the headers. 
+
+The way to find out what columns it has read in is just
+print data.columns
+
+If it doesn't show up the headers you expect then it probably hasn't been read in properly and I'd recommend looking at the read_table docs
+
+Hope this helps!
+
+Joey
+"""
+
+## Could do just this...
+ra = data['ra']
+dec = data['dec']
+w4snr = data['w4snr']
+## ... and then plot from there, or,
+
+#fig, ax = plt.subplots()
+#ax.scatter(ra, dec, edgecolor='')
+plt.scatter(ra, dec, edgecolor='', label='WISE w4')
+plt.show()
+xlabel('Right Ascension (J2000)')
+ylabel('Declination (J2000)')
+
+
+# Will do this...
+df = pd.DataFrame(data, columns=['ra', 'dec'])
+
+
+## Now we're getting into it... ;-)
+
+##
+##  gridsize  controls the number of hexagons in the x-direction
+##
+df.plot.hexbin(x='ra', y='dec', gridsize=25)
+df.plot.hexbin(x='ra', y='dec', C='z', reduce_C_function=np.maxgridsize=25)
+##
+df.plot.hexbin(x='a', y='b', C='z', reduce_C_function=np.max, gridsize=25)
+
+
+##
+## http://stackoverflow.com/questions/20105364/how-can-i-make-a-scatter-plot-colored-by-density-in-matplotlib
+##
+# Calculate the point density
+xy = np.vstack([ra,dec])
+
+## This next line just kills everything... :-/
+z = gaussian_kde(xy)(xy)
+
+# Sort the points by density, so that the densest points are plotted last
+#idx = z.argsort()
+#x, y, z = x[idx], y[idx], z[idx]
+
+#fig, ax = plt.subplots()
+#ax.scatter(x, y, c=z, s=50, edgecolor='')
+#plt.show()
+
+
+##
+## This is cool.. ;-)
+##
+## http://matplotlib.org/examples/shapes_and_collections/scatter_demo.html
+##
+##
