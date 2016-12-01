c1b483f020729e7b2b124a9d971ff71c631689db
npross <npross@lbl.gov>
2016-12-01 11:25:28 -0800

    plots/sky_distribution/Berghea/hexbin_forWISE.py

--- plots/sky_distribution/Berghea/hexbin_forWISE.py
+++ plots/sky_distribution/Berghea/hexbin_forWISE.py
@@ -9,8 +9,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 
 
-
-m = Basemap(projection='hammer',lon_0=0,resolution='c')
+m = Basemap(projection='hammer', lon_0=0, resolution='c')
 
 
 path = '/cos_pc19a_npr/data/WISE/W4/WISE_W4_W4SNRge3_W4MPROlt4.0_nohdr.tbl'
@@ -19,6 +18,21 @@ tbl = ascii.read(path)
 
 ra = tbl['ra']
 dec = tbl['dec']
+
+"""
+In [11]: ra.m
+ra.max   ra.mean  ra.meta  ra.min   ra.more  
+
+In [11]: ra.min
+Out[11]: <function Column.min>
+
+In [12]: print(ra.min)
+<built-in method min of Column object at 0x10ae41ac8>
+
+In [13]: print(ra.min())
+0.0019758
+"""
+
 
 x = ra
 y = dec
@@ -45,10 +59,9 @@ cb = plt.colorbar()
 cb.set_label('log10(N)')
 
 
+#plt.show()
+#plt.savefig('hexbin_forWISE_temp.png')
 
 fig = matplotlib.pyplot.gcf()
 fig.set_size_inches(18.5, 10.5)
-fig.savefig('test2png.png', dpi=100)
-
-#plt.show()
-plt.savefig('hexbin_forWISE_temp.png')
+fig.savefig('hexbin_forWISE_temp.png', dpi=100)
